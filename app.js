abc3e7baebe8b536f439528cd260112edad4b8f9
iamanudeep22 <anudeepandy22@gmail.com>
2024-08-21 14:22:43 +0530

    episode-3

--- app.js
+++ app.js
@@ -1,10 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const parent = React.createElement("div", {id:"parent"}, [
-    React.createElement("h1", {id:"child1"}, "this is react"),
-    React.createElement("h2", {id:"child2"}, "child2")
-])
+//React element
+const heading = (<h1 id="heading" className="heading">Namaste React from JSX!!</h1>);
+
+//React Components:- 
+//1) Class based components -> uses JS classes to create component (old)
+//2) Functional components -> uses JS functions to create component (new) -> just a JS functions which returns JSX
+
+//component composition:- composing one component inside another
+
+const Title = () => (<h1>ReactJS</h1>);
+
+const HeadingComponent = () => (
+    <div className="container">
+        <Title/>
+        {heading}
+        <h1 className="heading">Namaste React from Functional Component</h1>
+    </div>
+);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
+
+root.render(<HeadingComponent/>);
