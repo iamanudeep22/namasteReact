71a284c623497c3176b8d16d04e5cb988d39aa98
iamanudeep22 <anudeepandy22@gmail.com>
2024-08-28 14:00:32 +0530

    episode-4

--- app.js
+++ app.js
@@ -1,25 +1,314 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-//React element
-const heading = (<h1 id="heading" className="heading">Namaste React from JSX!!</h1>);
 
-//React Components:- 
-//1) Class based components -> uses JS classes to create component (old)
-//2) Functional components -> uses JS functions to create component (new) -> just a JS functions which returns JSX
+const Header = () => {
+    return (
+        <div className="header">
+            <div className="logo-container">
+                <img className="logo" src="https://thumbs.dreamstime.com/b/food-delivery-logo-design-template-134749604.jpg"/>
+            </div>
+            <div className="nav-items">
+                <ul>
+                    <li>Home</li>
+                    <li>About Us</li>
+                    <li>Contact Us</li>
+                    <li>Cart</li>
+                </ul>
+            </div>
+        </div>
+    )
+}
 
-//component composition:- composing one component inside another
 
-const Title = () => (<h1>ReactJS</h1>);
+const RestaurantCard = ({resData}) => {
 
-const HeadingComponent = () => (
-    <div className="container">
-        <Title/>
-        {heading}
-        <h1 className="heading">Namaste React from Functional Component</h1>
-    </div>
-);
+    const {cloudinaryImageId, name, cuisines, avgRating, costForTwo, sla} = resData?.info;
+    return (
+        <div className="res-card">
+            <img src={"https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/"+cloudinaryImageId}/>
+            <h3>{name}</h3>
+            <h4>{cuisines.join(", ")}</h4>
+            <h4>{avgRating} stars</h4>
+            <h4>{costForTwo}</h4>
+            <h4>{sla.slaString}</h4>
+        </div>
+    )
+}
+
+const resList = [
+    {
+      "info": {
+        "id": "393840",
+        "name": "Chinese Wok",
+        "cloudinaryImageId": "e0839ff574213e6f35b3899ebf1fc597",
+        "locality": "Chikka Lakshmaiah Layout",
+        "areaName": "Adugodi",
+        "costForTwo": "₹250 for two",
+        "cuisines": [
+          "Chinese",
+          "Asian",
+          "Tibetan",
+          "Desserts"
+        ],
+        "avgRating": 4.2,
+        "parentId": "61955",
+        "avgRatingString": "4.2",
+        "totalRatingsString": "1K+",
+        "sla": {
+          "deliveryTime": 32,
+          "lastMileTravel": 2.5,
+          "serviceability": "SERVICEABLE",
+          "slaString": "30-35 mins",
+          "lastMileTravelString": "2.5 km",
+          "iconType": "ICON_TYPE_EMPTY"
+        },
+      },
+    },
+    {
+      "info": {
+        "id": "5934",
+        "name": "Burger King",
+        "cloudinaryImageId": "RX_THUMBNAIL/IMAGES/VENDOR/2024/6/11/7f76a072-c1bc-4d74-ac56-33e0eea20c1e_5934.JPG",
+        "locality": "Koramangala",
+        "areaName": "Koramangala",
+        "costForTwo": "₹350 for two",
+        "cuisines": [
+          "Burgers",
+          "American"
+        ],
+        "avgRating": 4.3,
+        "parentId": "166",
+        "avgRatingString": "4.3",
+        "totalRatingsString": "10K+",
+        "sla": {
+          "deliveryTime": 20,
+          "lastMileTravel": 1.4,
+          "serviceability": "SERVICEABLE",
+          "slaString": "15-20 mins",
+          "lastMileTravelString": "1.4 km",
+          "iconType": "ICON_TYPE_EMPTY"
+        },
+       
+        }
+      
+    },
+    {
+      "info": {
+        "id": "93741",
+        "name": "Subway",
+        "cloudinaryImageId": "RX_THUMBNAIL/IMAGES/VENDOR/2024/8/2/877b99a7-fdcc-40c6-a19a-684a746243aa_93741.jpg",
+        "locality": "The Nexus Mall",
+        "areaName": "Koramangala",
+        "costForTwo": "₹350 for two",
+        "cuisines": [
+          "Salads",
+          "Snacks",
+          "Desserts",
+          "Beverages"
+        ],
+        "avgRating": 4,
+        "parentId": "2",
+        "avgRatingString": "4.0",
+        "totalRatingsString": "5K+",
+        "sla": {
+          "deliveryTime": 31,
+          "lastMileTravel": 1.6,
+          "serviceability": "SERVICEABLE",
+          "slaString": "30-35 mins",
+          "lastMileTravelString": "1.6 km",
+          "iconType": "ICON_TYPE_EMPTY"
+        },
+        
+      }
+      
+    },
+    {
+      "info": {
+        "id": "613421",
+        "name": "Kwality Walls Ice Cream and More",
+        "cloudinaryImageId": "RX_THUMBNAIL/IMAGES/VENDOR/2024/6/13/b3739937-c8ca-40ea-96cd-03b049b4600e_613421.JPG",
+        "locality": "6th Block",
+        "areaName": "Koramangala",
+        "costForTwo": "₹200 for two",
+        "cuisines": [
+          "Desserts",
+          "Ice Cream",
+          "Ice Cream Cakes"
+        ],
+        "avgRating": 4.8,
+        "veg": true,
+        "parentId": "582",
+        "avgRatingString": "4.8",
+        "totalRatingsString": "100+",
+        "sla": {
+          "deliveryTime": 16,
+          "lastMileTravel": 0.7,
+          "serviceability": "SERVICEABLE",
+          "slaString": "15-20 mins",
+          "lastMileTravelString": "0.7 km",
+          "iconType": "ICON_TYPE_EMPTY"
+        },
+    
+      }
+    },
+    {
+      "info": {
+        "id": "581809",
+        "name": "UBQ by Barbeque Nation",
+        "cloudinaryImageId": "muaktnk5xb3zop4bvj6l",
+        "locality": "7th Block",
+        "areaName": "Koramangala",
+        "costForTwo": "₹300 for two",
+        "cuisines": [
+          "North Indian",
+          "Barbecue",
+          "Biryani",
+          "Kebabs",
+          "Mughlai",
+          "Desserts"
+        ],
+        "avgRating": 3.9,
+        "parentId": "10804",
+        "avgRatingString": "3.9",
+        "totalRatingsString": "100+",
+        "sla": {
+          "deliveryTime": 29,
+          "lastMileTravel": 1.3,
+          "serviceability": "SERVICEABLE",
+          "slaString": "25-30 mins",
+          "lastMileTravelString": "1.3 km",
+          "iconType": "ICON_TYPE_EMPTY"
+        },
+       
+      }
+    },
+    {
+      "info": {
+        "id": "716672",
+        "name": "Chakum Chukum",
+        "cloudinaryImageId": "8c969f1587932a41f5d73470614ab742",
+        "locality": "B B M P South",
+        "areaName": "HSR",
+        "costForTwo": "₹300 for two",
+        "cuisines": [
+          "Fast Food",
+          "Biryani",
+          "North Indian",
+          "Bengali",
+          "Mughlai"
+        ],
+        "avgRating": 4.3,
+        "parentId": "2187",
+        "avgRatingString": "4.3",
+        "totalRatingsString": "500+",
+        "sla": {
+          "deliveryTime": 34,
+          "lastMileTravel": 4.4,
+          "serviceability": "SERVICEABLE",
+          "slaString": "30-35 mins",
+          "lastMileTravelString": "4.4 km",
+          "iconType": "ICON_TYPE_EMPTY"
+        },
+       
+      }
+      
+    },
+    {
+      "info": {
+        "id": "375041",
+        "name": "Andhra Gunpowder",
+        "cloudinaryImageId": "byilgyrcfz690ryoasww",
+        "locality": "6th Block",
+        "areaName": "Koramangala",
+        "costForTwo": "₹350 for two",
+        "cuisines": [
+          "Andhra",
+          "Biryani",
+          "South Indian"
+        ],
+        "avgRating": 4.5,
+        "parentId": "10496",
+        "avgRatingString": "4.5",
+        "totalRatingsString": "5K+",
+        "sla": {
+          "deliveryTime": 19,
+          "lastMileTravel": 0.1,
+          "serviceability": "SERVICEABLE",
+          "slaString": "15-20 mins",
+          "lastMileTravelString": "0.1 km",
+          "iconType": "ICON_TYPE_EMPTY"
+        },
+       
+       
+       
+      }
+      
+    },
+    {
+      "info": {
+        "id": "18973",
+        "name": "Nandhana Palace",
+        "cloudinaryImageId": "195876a3181ef63f76e45e3a7b49b585",
+        "locality": "Koramangala",
+        "areaName": "Koramangala",
+        "costForTwo": "₹500 for two",
+        "cuisines": [
+          "Biryani",
+          "Andhra",
+          "South Indian",
+          "North Indian"
+        ],
+        "avgRating": 4.3,
+        "parentId": "2120",
+        "avgRatingString": "4.3",
+        "totalRatingsString": "10K+",
+        "sla": {
+          "deliveryTime": 17,
+          "lastMileTravel": 0.6,
+          "serviceability": "SERVICEABLE",
+          "slaString": "15-20 mins",
+          "lastMileTravelString": "0.6 km",
+          "iconType": "ICON_TYPE_EMPTY"
+        },
+        
+      },
+      
+    }
+  ]
+
+const Body = () => {
+    return (
+        <div className="body">
+            <div className="search">
+                <input type="text"></input>
+                <button></button>
+            </div>
+            <div className="res-container">
+                {
+                    resList.map((Restaurant) => <RestaurantCard key={Restaurant.info.id} resData={Restaurant}/>)
+                }
+
+            </div>
+        </div>
+    )
+}
+
+
+
+const AppLayoutComponent = () => {
+    return (
+        <div className="app">
+            <Header/>
+            <Body/>
+        </div>
+    )
+}
+
+
+
+
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(<HeadingComponent/>);
+root.render(<AppLayoutComponent/>);
